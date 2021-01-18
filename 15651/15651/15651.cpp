61ff3e916b45066cb83b8d48a9aa02e67e273952
Shin-jisoo <wltn114300@gmail.com>
2021-01-18 23:18:44 +0900

    code upload

---
+++ 15651/15651/15651.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#define MAX 9
+
+using namespace std;
+
+int n, m;
+int arr[MAX] = { 0, };
+bool visited[MAX] = { 0, };
+
+void dfs(int cnt) {
+	if (cnt == m) {
+		for (int i = 0;i < m;i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	// 같은 수를 여러 번 골라도 되므로
+	// if(!visited[i]) 생략
+	for (int i = 1;i <= n;i++) {
+		visited[i] = true;
+		arr[cnt] = i;
+		dfs(cnt + 1);
+		visited[i] = false;
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	dfs(0);
+}
