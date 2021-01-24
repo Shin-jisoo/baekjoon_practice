5035c5e653e015af8cf180e7fa02e2490631c6c0
Shin-jisoo <wltn114300@gmail.com>
2021-01-24 17:57:30 +0900

    code change

--- 9663/9663/9663.cpp
+++ 9663/9663/9663.cpp
@@ -1,43 +1,47 @@
 #include <iostream>
-#include <bits/stdc++.h>
+#include <algorithm>
 
 using namespace std;
 
-int N;
-int vx[16], vy[16];
+int n;
+int arr[16] = { 0, };
+int cnt = 0;
 
-int go(int y, int x) {
+//back tracking 가지치기
+bool check(int l) {
 
-	for (int i = 0;i < y;i++) {
-		if (x == vx[i]) return 0; //세로끼리 겹침
-		if (y == vy[i]) return 0; //가로끼리 겹침
-		if (abs(x - vx[i]) == abs(y - vy[i])) return 0; //대각선 겹침
+	//해당 열까지 for 문 사용
+	for (int i = 0;i < l;i++) {
+		//같은 행에 있거나, 대각선에 있는 경우 false
+		if (arr[l] == arr[i] || abs(arr[l] - arr[i]) == l - i)
+			return false;
 	}
 
-	//종료 조건
-	if (y == N - 1) {
-		return 1;
-	}
-
-	vx[x] = x;
-	vy[y] = y;
-
-	int r = 0;
-	for (int i = 0;i < N;i++) {
-		r += go(y + 1, i);
-	}
-	return r;
+	//나머지 true
+	return true;
 }
 
-int main() {
-	cin >> N;
-	//scanf("%d", &N);
+void func(int k) {
 
-	int r = 0;
-	for (int i = 0;i < N;i++) {
-		r += go(0, i);
+	//완성된 경우 cnt 1증가
+	if (k == n) {
+		cnt++;
 	}
 
-	cout << r;
-	return 0;
+	else {
+		for (int i = 1;i <= n;i++) {
+			//k 번째에 i 넣어주기
+			arr[k] = i;
+
+			//check 해서 true 인 경우 다음번째에 수 넣어주기
+			if (check(k)) func(k + 1);
+		}
+	}
+	}
+
+int main() {
+	cin >> n;
+	func(0);
+
+	cout << cnt;
 }
