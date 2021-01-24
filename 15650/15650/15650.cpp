549f7887f13b3b2a6b856cc376262d07a3ae8c24
Shin-jisoo <wltn114300@gmail.com>
2021-01-24 17:26:57 +0900

    code change

--- 15650/15650/15650.cpp
+++ 15650/15650/15650.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-#define MAX 9
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int n, m;
-int arr[MAX] = { 0, };
-int visited[MAX] = { 0, }; // 사용되었는지 확인하기 위한 배열
+int arr[9] = { 0, };
+bool isused[9] = { 0, };
 
-void dfs(int num, int cnt) {
-	if (cnt == m) {
+void func(int num,int k) {
+	if (k == m) {
 		for (int i = 0;i < m;i++) {
 			cout << arr[i] << ' ';
 		}
@@ -16,20 +17,19 @@ void dfs(int num, int cnt) {
 		return;
 	}
 
-	// 두번째 숫자는 반드시 첫번째 숫자보다 크도록, 세번째 숫자는 반드시 두번쨰 숫자보다 크도록 설정
+	// i=num 으로 시작해서 두번째 숫자는 반드시 첫번째 숫자보다 크도록
+	// 세번째 숫자는 반드시 두번째 숫자보다 크도록 함
 	for (int i = num;i <= n;i++) {
-		if (!visited[i]) {
-			visited[i] = true;
-			arr[cnt] = i;
-
-			// 현재 뽑은 원소이 인전 값들은 고려하지 않게끔, for문의 i값을 함께 넘겨줌
-			dfs(i + 1, cnt + 1);
-			visited[i] = false;
+		if (!isused[i]) {
+			arr[k] = i;
+			isused[i] = true;
+			func(i + 1, k + 1);
+			isused[i] = false;
 		}
 	}
 }
 
 int main() {
 	cin >> n >> m;
-	dfs(1, 0);
+	func(1,0);
 }
